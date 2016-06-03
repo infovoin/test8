f1b4f534c16bf07ddd4a06e232768601f9c70de5
Artur <infovoin@gmail.com>
2016-06-03 17:59:51 +0300

    готово

--- views/test8/show-statistics.php
+++ views/test8/show-statistics.php
@@ -1,11 +1,7 @@
 <?php
-use yii\helpers\Html;
 use miloschuman\highcharts\Highcharts;
-use miloschuman\highcharts\Highstock;
-use miloschuman\highcharts\SeriesDataHelper;
 
 ?>
-
 
 <div class="site-index">
     <div class="jumbotron">
@@ -13,70 +9,120 @@ use miloschuman\highcharts\SeriesDataHelper;
     </div>
 </div>
 
+
+<? foreach ($videos as $video): ?>
+<?php $video['statistics'] = json_decode($video['statistics']); ?>
 <div class="row">
-    <div class="col-lg-12">
+    <div class="col-lg-5">
+        <p>
+            <img src="<?= $video->video_thumbnails_url ?>" width="480" height="360"></p>
+    </div>
+    <div class="col-lg-6">
         <div>
-            <?= $videoStatistics->videoEmbedHtml ?>
-            <?= Highcharts::widget([
-                'options' => [
-                    'title' => ['text' => 'Statistics'],
-                    'xAxis' => [
-                        'categories' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-                    ],
-                    'yAxis' => [
-                        'title' => ['text' => 'growth index']
-                    ],
-                    'series' => [
-                        ['name' => 'Views count', 'data' => [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]],
-                        ['name' => 'Comments count', 'data' => [0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]],
-                        ['name' => 'Subscribers count', 'data' => [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]],
-                        ['name' => 'Likes', 'data' => [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]],
-                        ['name' => 'Dislikes', 'data' => [7.0, 6.9, 9.5, 14.5, 11.9, 15.2, 17.0, 16.6, 20.1, 14.1, 8.6, 2.5]],
-
-                    ]
-                ]
-            ]);
-
-            ?>
+            <ul class="nav nav-tabs" data-tabs="tabs">
+                <li class="active"><a data-toggle="tab" href="#<?= $video->youtube_video_id ?>_view_count">Views
+                        count</a></li>
+                <li><a data-toggle="tab" href="#<?= $video->youtube_video_id ?>_comment_count">Comments count</a>
+                </li>
+                <li><a data-toggle="tab" href="#<?= $video->youtube_video_id ?>_like_dislike">like / dislike</a>
+                </li>
+                <li><a data-toggle="tab" href="#<?= $video->youtube_video_id ?>_subscriber_count">Subscribers
+                        count</a>
+                </li>
+            </ul>
+            <div class="tab-content">
+                <div class="tab-pane active" id="<?= $video->youtube_video_id ?>_view_count">
+                    <?php echo Highcharts::widget([
+                        'options' => [
+                            'title' => ['text' => 'Views count'],
+                            'xAxis' => [
+                                'categories' => []
+                            ],
+                            'yAxis' => [
+                                'title' => ['text' => 'growth index']
+                            ],
+                            'series' => [
+                                ['name' => 'Views count', 'data' => $video['statistics']->video_view_count],
+                            ]
+                        ]
+                    ]); ?>
+                </div>
+                <div class="tab-pane" id="<?= $video->youtube_video_id ?>_comment_count">
+                    <?php echo Highcharts::widget([
+                        'options' => [
+                            'title' => ['text' => 'Comments count'],
+                            'xAxis' => [
+                                'categories' => []
+                            ],
+                            'yAxis' => [
+                                'title' => ['text' => 'growth index']
+                            ],
+                            'series' => [
+                                ['name' => 'Comment count', 'data' => $video['statistics']->video_comment_count],
+                            ]
+                        ]
+                    ]); ?>
+                </div>
+                <div class="tab-pane" id="<?= $video->youtube_video_id ?>_like_dislike">
+                    <?php echo Highcharts::widget([
+                        'options' => [
+                            'title' => ['text' => 'Likes / dislikes'],
+                            'xAxis' => [
+                                'categories' => []
+                            ],
+                            'yAxis' => [
+                                'title' => ['text' => 'growth index']
+                            ],
+                            'series' => [
+                                ['name' => 'Like count', 'data' => $video['statistics']->video_like_count],
+                                ['name' => 'Dislike count', 'data' => $video['statistics']->video_dislike_count],
+                            ]
+                        ]
+                    ]);
+                    ?>
+                </div>
+                <div class="tab-pane" id="<?= $video->youtube_video_id ?>_subscriber_count">
+                    <?php echo Highcharts::widget([
+                        'options' => [
+                            'title' => ['text' => 'Subscribers count'],
+                            'xAxis' => [
+                                'categories' => []
+                            ],
+                            'yAxis' => [
+                                'title' => ['text' => 'growth index']
+                            ],
+                            'series' => [
+                                ['name' => 'Subscriber count', 'data' => $video['statistics']->channel_subscriber_count],
+                            ]
+                        ]
+                    ]); ?>
+                </div>
+            </div>
         </div>
-
-        <table class="table table-striped">
-            <tr>
-                <td>Video title:</td>
-                <td><?= $videoStatistics->videoTitle ?></td>
-            </tr>
-            <tr>
-                <td>Channel title:</td>
-                <td><?= $videoStatistics->channelTitle ?></td>
-            </tr>
-            <tr>
-                <td>Video description:</td>
-                <td><?= $videoStatistics->videoDescription ?></td>
-            </tr>
-            <tr>
-                <td>Video thumbnail:</td>
-                <td><?= $videoStatistics->videoThumbnailsUrl ?></td>
-            </tr>
-            <tr>
-                <td>Views count:</td>
-                <td><?= $videoStatistics->videoViewCount ?></td>
-            </tr>
-            <tr>
-                <td>Comments count:</td>
-                <td><?= $videoStatistics->videoCommentCount ?></td>
-            </tr>
-            <tr>
-                <td>Likes/Dislikes</td>
-                <td><?= $videoStatistics->videolikeCount ?> / <?= $videoStatistics->videoDislikeCount ?></td>
-            </tr>
-            <tr>
-                <td>Subscribers count:</td>
-                <td><?= $videoStatistics->channelSubscriberCount ?></td>
-            </tr>
-        </table>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <table class="table table-striped">
+                <tr>
+                    <td>Video title:</td>
+                    <td><?= $video->video_title ?></td>
+                </tr>
+                <tr>
+                    <td>Channel title:</td>
+                    <td><?= $video->channel_title ?></td>
+                </tr>
+                <tr>
+                    <td>Video description:</td>
+                    <td><?= $video->video_description ?></td>
+                </tr>
+            </table>
+        </div>
     </div>
     <hr>
+    <div style="height: 70px;"></div>
+    <?php endforeach; ?>
+
+
 </div>
 
 
